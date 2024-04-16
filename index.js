5fafc760bd335bbaede9d1933d2d17b1be211b32
Lakshan Tharaka <tharakahitibandara@gmail.com>
2024-04-16 14:46:07 +0530

    ok

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
