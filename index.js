7b6f1aa497738d9d90dc681627a81998e8d4782b
Lakshan Tharaka <tharakahitibandara@gmail.com>
2024-04-17 17:26:53 +0530

    ok

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// This line must come before importing any instrumented module.
+const tracer = require('dd-trace').init()
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
