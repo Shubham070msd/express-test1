b2764c987f6a22a211fd278cff7da66de75c9e7b
Manjunath Huddar <67737064+Shubham070msd@users.noreply.github.com>
2024-04-22 11:33:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3009;
 
 app.get('/ping', (req, res) => {
     res.send('pong');
