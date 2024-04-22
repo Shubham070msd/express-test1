4af49aec57b9eff0ecb8fb22bcfd6ce224d015c3
Manjunath Huddar <67737064+Shubham070msd@users.noreply.github.com>
2024-04-22 11:53:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3009;
 
 app.get('/ping', (req, res) => {
-    res.send('pinged');
+    res.send('pinged request incoming');
 });
 
 app.listen(port, () => {
